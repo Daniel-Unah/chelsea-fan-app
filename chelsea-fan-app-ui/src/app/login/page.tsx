eb529382c1d12fea28222501d34b18f4bbbca5ff
Daniel <106980147+Daniel-Unah@users.noreply.github.com>
2025-07-10 19:45:12 -0700

    Fix apostrophe escaping in login page

--- chelsea-fan-app-ui/src/app/login/page.tsx
+++ chelsea-fan-app-ui/src/app/login/page.tsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
         </button>
         <div className="mt-4 text-center">
           <p className="text-gray-600 dark:text-gray-400">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="text-blue-600 hover:text-blue-700 underline">
               Sign up here
             </Link>
